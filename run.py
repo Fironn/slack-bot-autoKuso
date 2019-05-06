4bb9c4a47b4dbd65981810a2b318f16f51951246
Fironn <shihomihashi@gmail.com>
2019-05-06 11:46:42 +0900

    bot-kuso

--- run.py
+++ run.py
@@ -1,24 +1,11 @@
-# -*- coding: utf-8 -*-
-import json
-import webapp2
+# coding: utf-8
 
-class EchoHandler(webapp2.RequestHandler):
-    VERIFICATION_TOKEN = 'xoxp-460747175155-460238652017-625527914960-2fd8a0980cd6bbfefae50cc6c551867d'
+from slackbot.bot import Bot
 
-    def post(self):
-        body = json.loads(self.request.body)
-        if body['token'] != self.VERIFICATION_TOKEN:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.status = 403
-            self.response.write('403 Forbidden')
-            return
-        if body['type'] == 'url_verification':
-            self.verify_url(body)
+def main():
+    bot = Bot()
+    bot.run()
 
-    def verify_url(self, params):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(params['challenge'])
-
-app = webapp2.WSGIApplication([
-    ('/echo-bot-hook', EchoHandler)
-], debug=True)
+if __name__ == "__main__":
+    print('start slackbot')
+    main()
